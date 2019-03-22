5b67692dfce8b971cc9c17c4037a15a9a910a1d6
expencive <expencive@bk.ru>
2019-03-22 21:39:43 +0300

    RecipesApp

---
+++ Recipes/app/src/main/java/expencive/vk/com/recipes/requests/ServiceGenerator.java
@@ -0,0 +1,4 @@
+package expencive.vk.com.recipes.requests;
+
+public class ServiceGenerator {
+}
---
+++ Recipes/app/src/main/java/expencive/vk/com/recipes/util/Constants.java
@@ -0,0 +1,7 @@
+package expencive.vk.com.recipes.util;
+
+public class Constants {
+
+    public static final String BASE_URL = "https://www.food2fork.com";
+    public static final String API_KEY = "e6e81685e105eb03edb4336f27d3ee60";
+}
