8595de5fd4d767157ea402034c0d55bf7ea2ed7d
expencive <expencive@bk.ru>
2019-03-23 10:47:12 +0300

    RecipeApp

--- Recipes/app/src/main/java/expencive/vk/com/recipes/requests/RecipeApi.java
+++ Recipes/app/src/main/java/expencive/vk/com/recipes/requests/RecipeApi.java
@@ -1,4 +1,26 @@
 package expencive.vk.com.recipes.requests;
 
+import expencive.vk.com.recipes.requests.responses.RecipeResponse;
+import expencive.vk.com.recipes.requests.responses.RecipeSearchResponse;
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
 public interface RecipeApi {
+
+    // SEARCH
+    @GET("api/search")
+    Call<RecipeSearchResponse> searchRecipe(
+            @Query("key") String key,
+            @Query("q") String query,
+            @Query("page") String page
+    );
+
+    // GET SPECIFIC RECIPE
+    @GET("api/get")
+    Call<RecipeResponse> getRecipe(
+            @Query("key") String key,
+            @Query("rId") String recipe_id
+    );
+
 }
--- Recipes/app/src/main/java/expencive/vk/com/recipes/requests/ServiceGenerator.java
+++ Recipes/app/src/main/java/expencive/vk/com/recipes/requests/ServiceGenerator.java
@@ -6,15 +6,16 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ServiceGenerator {
 
-    public static Retrofit.Builder retrofitBuilder = new Retrofit.Builder()
-            .baseUrl(Constants.BASE_URL)
-            .addConverterFactory(GsonConverterFactory.create());
+    private static Retrofit.Builder retrofitBuilder =
+            new Retrofit.Builder()
+                    .baseUrl(Constants.BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create());
 
     private static Retrofit retrofit = retrofitBuilder.build();
 
     private static RecipeApi recipeApi = retrofit.create(RecipeApi.class);
 
-    public static RecipeApi getRecipeApi(){
+    public static RecipeApi getRecipeApi() {
         return recipeApi;
     }
 }
