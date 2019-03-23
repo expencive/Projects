22967696154646ec1f5e75df03cca71450f63553
expencive <expencive@bk.ru>
2019-03-23 11:36:59 +0300

    RecipeApp

--- Recipes/app/src/main/java/expencive/vk/com/recipes/RecipeListActivity.java
+++ Recipes/app/src/main/java/expencive/vk/com/recipes/RecipeListActivity.java
@@ -2,10 +2,26 @@ package expencive.vk.com.recipes;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import expencive.vk.com.recipes.models.Recipe;
+import expencive.vk.com.recipes.requests.RecipeApi;
+import expencive.vk.com.recipes.requests.ServiceGenerator;
+import expencive.vk.com.recipes.requests.responses.RecipeResponse;
+import expencive.vk.com.recipes.requests.responses.RecipeSearchResponse;
+import expencive.vk.com.recipes.util.Constants;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 
 public class RecipeListActivity extends BaseActivity {
+    private static final String TAG = "RecipeListActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,13 +31,69 @@ public class RecipeListActivity extends BaseActivity {
         findViewById(R.id.test).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(mProgressBar.getVisibility() == View.VISIBLE){
-                    showProgressBar(false);
-                }
-                else{
-                    showProgressBar(true);
-                }
+                testRetrofitRequest();
             }
         });
     }
+
+    private void testRetrofitRequest() {
+        RecipeApi recipeApi = ServiceGenerator.getRecipeApi();
+
+//        Call<RecipeSearchResponse> responseCall = recipeApi.searchRecipe(Constants.API_KEY,
+//                "chicken breast", "1");
+//
+//        responseCall.enqueue(new Callback<RecipeSearchResponse>() {
+//            @Override
+//            public void onResponse(Call<RecipeSearchResponse> call, Response<RecipeSearchResponse> response) {
+//                Log.d(TAG, "onResponse: server response: " + response.toString());
+//                if (response.code() == 200) {
+//                    Log.d(TAG, "onResponse: " + response.body().toString());
+//                    List<Recipe> recipes = new ArrayList<>(response.body().getRecipes());
+//
+//                    for (Recipe recipe: recipes) {
+//                        Log.d(TAG, "onResponse: " + recipe.getTitle());
+//                    }
+//                }else {
+//                    try {
+//                        Log.d(TAG, "onResponse: " + response.errorBody().string());
+//
+//                    }catch (IOException e){
+//                        e.printStackTrace();
+//                    }
+//                }
+//
+//            }
+//
+//            @Override
+//            public void onFailure(Call<RecipeSearchResponse> call, Throwable t) {
+//
+//            }
+//        });
+
+        Call<RecipeResponse> responseCall = recipeApi.getRecipe(Constants.API_KEY,
+                "8c0314");
+
+        responseCall.enqueue(new Callback<RecipeResponse>() {
+            @Override
+            public void onResponse(Call<RecipeResponse> call, Response<RecipeResponse> response) {
+                Log.d(TAG, "onResponse: Server Response: " + response.toString());
+                if (response.code() == 200) {
+                    Recipe recipe = response.body().getRecipe();
+                    Log.d(TAG, "onResponse: " + recipe.toString());
+                }
+                else{
+                    try {
+                        Log.d(TAG, "onResponse: " + response.errorBody().string());
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+            @Override
+            public void onFailure(Call<RecipeResponse> call, Throwable t) {
+                Log.d(TAG, "onResponse: ERROR: " + t.getMessage());
+            }
+        });
+    }
+
 }
