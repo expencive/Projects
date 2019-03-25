85155eafc9486ff025100c76582568f37a3bef1c
expencive <expencive@bk.ru>
2019-03-25 20:26:36 +0300

    RecipeApp

---
+++ Recipes/app/src/main/java/expencive/vk/com/recipes/RecipeActivity.java
@@ -0,0 +1,39 @@
+package expencive.vk.com.recipes;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.widget.AppCompatImageView;
+import expencive.vk.com.recipes.models.Recipe;
+
+public class RecipeActivity extends BaseActivity {
+
+    //UI components
+    private AppCompatImageView mRecipeImage;
+    private TextView mRecipeTitle, mRecipeRank;
+    private LinearLayout mRecipeIngridientsContainer;
+    private ScrollView mScrollView;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recipe);
+        mRecipeImage = findViewById(R.id.recipe_image);
+        mRecipeTitle = findViewById(R.id.recipe_title);
+        mRecipeRank = findViewById(R.id.recipe_social_score);
+        mRecipeIngridientsContainer = findViewById(R.id.ingredients_container);
+        mScrollView = findViewById(R.id.parent);
+
+        getIncomingIntent();
+    }
+
+    private void getIncomingIntent(){
+        if (getIntent().hasExtra("recipe")){
+            Recipe recipe = getIntent().getParcelableExtra("recipe");
+        }
+    }
+}
