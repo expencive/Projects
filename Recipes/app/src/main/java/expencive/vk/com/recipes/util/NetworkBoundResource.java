65cb3f4072ec48f8c9ba94aeee41af50666e9070
expencive <expencive@bk.ru>
2019-04-03 13:20:27 +0300

    Recipes

---
+++ Recipes/app/src/main/java/expencive/vk/com/recipes/util/NetworkBoundResource.java
@@ -0,0 +1,39 @@
+package expencive.vk.com.recipes.util;
+
+import androidx.annotation.MainThread;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.annotation.WorkerThread;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MediatorLiveData;
+import expencive.vk.com.recipes.requests.responses.ApiResponse;
+
+// CacheObject: Type for the Resource data. (database cache)
+// RequestObject: Type for the API response. (network request)
+public abstract class NetworkBoundResource<CacheObject, RequestObject> {
+
+    private MediatorLiveData<Resource<CacheObject>> results = new MediatorLiveData<>();
+
+    // Called to save the result of the API response into the database.
+    @WorkerThread
+    protected abstract void saveCallResult(@NonNull RequestObject item);
+
+    // Called with the data in the database to decide whether to fetch
+    // potentially updated data from the network.
+    @MainThread
+    protected abstract boolean shouldFetch(@Nullable CacheObject data);
+
+    // Called to get the cached data from the database.
+    @NonNull @MainThread
+    protected abstract LiveData<CacheObject> loadFromDb();
+
+    // Called to create the API call.
+    @NonNull @MainThread
+    protected abstract LiveData<ApiResponse<RequestObject>> createCall();
+
+    // Returns a LiveData object that represents the resource that's implemented
+    // in the base class.
+    public final LiveData<Resource<CacheObject>> getAsLiveData(){
+        return results;
+    };
+}
