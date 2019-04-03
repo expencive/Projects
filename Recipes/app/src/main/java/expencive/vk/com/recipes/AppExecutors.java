9b682cb783fda5d3ebe85d29568f6dee768e7271
expencive <expencive@bk.ru>
2019-04-03 14:47:41 +0300

    Update AppExecutors.java

--- Recipes/app/src/main/java/expencive/vk/com/recipes/AppExecutors.java
+++ Recipes/app/src/main/java/expencive/vk/com/recipes/AppExecutors.java
@@ -1,24 +1,53 @@
 package expencive.vk.com.recipes;
 
+import android.os.Handler;
+import android.os.Looper;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 
+import androidx.annotation.NonNull;
+
 public class AppExecutors {
 
     private static AppExecutors instance;
-    public static AppExecutors getInstance() {
-        if (instance==null) {
+
+    public static AppExecutors getInstance(){
+        if(instance == null){
             instance = new AppExecutors();
         }
         return instance;
     }
 
-    private final ScheduledExecutorService mNetworkIO = Executors.newScheduledThreadPool(3);
+    private final Executor mDiskIO = Executors.newSingleThreadExecutor();
 
-    public ScheduledExecutorService networkIO() {
-        return mNetworkIO;
+    private final Executor mMainThreadExecutor = new MainThreadExecutor();
+
+
+    public Executor diskIO(){
+        return mDiskIO;
     }
 
+    public Executor mainThread(){
+        return mMainThreadExecutor;
+    }
 
+    private static class MainThreadExecutor implements Executor{
+
+        private Handler mainThreadHandler = new Handler(Looper.getMainLooper());
+
+        @Override
+        public void execute(@NonNull Runnable command) {
+            mainThreadHandler.post(command);
+        }
+    }
 }
+
+//    private final ScheduledExecutorService mNetworkIO = Executors.newScheduledThreadPool(3);
+//
+//    public ScheduledExecutorService networkIO() {
+//        return mNetworkIO;
+//    }
+
+
+
