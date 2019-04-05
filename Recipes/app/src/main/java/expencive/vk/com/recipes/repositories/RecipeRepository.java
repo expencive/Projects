c124b1e70f834d5ba438e77c3030a825c579373c
expencive <expencive@bk.ru>
2019-04-05 14:01:57 +0300

    Update RecipeRepository.java

--- Recipes/app/src/main/java/expencive/vk/com/recipes/repositories/RecipeRepository.java
+++ Recipes/app/src/main/java/expencive/vk/com/recipes/repositories/RecipeRepository.java
@@ -65,7 +65,6 @@ public class RecipeRepository {
                                     recipes[index].getSocial_rank()
                             );
                         }
-                        
                         index++;
                     }
 
